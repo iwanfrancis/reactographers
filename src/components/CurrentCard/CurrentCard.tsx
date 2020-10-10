6729716bd849266db6f552f55379ea813e14c80b
Iwan <iwanfrancis@gmail.com>
2020-10-10 21:13:51 +0100

    Tidy up CurrentCard rendering

--- src/components/CurrentCard/CurrentCard.tsx
+++ src/components/CurrentCard/CurrentCard.tsx
@@ -5,45 +5,59 @@ import { Terrain } from '../../constants/Terrains';
 import styles from './CurrentCard.module.scss';
 
 export interface Props {
-    card: Card;
-    setCurrentTerrain: (terrain: Terrain) => any;
-    setCurrentShape: (shape: Shape) => any;
+  card: Card;
+  setCurrentTerrain: (terrain: Terrain) => any;
+  setCurrentShape: (shape: Shape) => any;
 }
 
 export default class CurrentCard extends React.PureComponent<Props> {
 
-    render() {
-        const { card, setCurrentTerrain = ()=>{}, setCurrentShape = ()=>{} } = this.props;
+  renderTerrainOptions(terrains: Terrain[]) {
+    const { setCurrentTerrain = ()=>{} } = this.props;
+    return (
+      <div className={styles.terrains}>
+        {terrains.map(terrain => {
+          return(
+            <div key={terrain} className={styles.terrain} onClick={() => setCurrentTerrain(terrain)}>
+              {terrain}
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
 
-        if (isShapeCard(card)) {
-            const shapeCard = card as ShapeCard;
-            return (
-                <div className={styles.card}>
-                    <div className={styles.header}>
-                        {shapeCard.time}   {shapeCard.name}
-                    </div>
-                    <div className={styles.options}>
-                        <div className={styles.terrains}>
-                            {shapeCard.terrains.map(terrain => {
-                                return(
-                                    <div key={terrain} className={styles.terrain} onClick={() => setCurrentTerrain(terrain)}>
-                                        {terrain}
-                                    </div>
-                                )
-                            })}
-                        </div>
-                        <div className={styles.shapes}>
-                            {shapeCard.shapes.map((shape, i) => {
-                                    return(
-                                        <div key={i} className={styles.shape} onClick={() => setCurrentShape(shape)}>
-                                            shape
-                                        </div>
-                                    )
-                                })}
-                        </div>
-                    </div>
-                </div>
+  renderShapeOptions(shapes: Shape[]) {
+    const { setCurrentShape = ()=>{} } = this.props;
+    return (
+      <div className={styles.shapes}>
+        {shapes.map((shape, i) => {
+            return(
+              <div key={i} className={styles.shape} onClick={() => setCurrentShape(shape)}>
+                shape
+              </div>
             )
-        }
+          })}
+      </div>
+    )
+  }
+
+  render() {
+    const { card } = this.props;
+
+    if (isShapeCard(card)) {
+      const shapeCard = card as ShapeCard;
+      return (
+        <div className={styles.card}>
+          <div className={styles.header}>
+            {shapeCard.time}   {shapeCard.name}
+          </div>
+          <div className={styles.options}>
+            {this.renderTerrainOptions(shapeCard.terrains)}
+            {this.renderShapeOptions(shapeCard.shapes)}
+          </div>
+        </div>
+      )
     }
+  }
 }
