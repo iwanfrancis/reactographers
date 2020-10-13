f22bfac5bcc75ddd7545faf80d2f0661dd06955f
Iwan <iwanfrancis@gmail.com>
2020-10-13 19:05:05 +0100

    Add edicts enum and seasons

---
+++ src/constants/Edicts.ts
@@ -0,0 +1,6 @@
+export enum Edict {
+  A,
+  B,
+  C,
+  D
+}
---
+++ src/constants/Seasons.ts
@@ -0,0 +1,40 @@
+import { Edict } from "./Edicts";
+
+export interface Season {
+  name: string;
+  length: number;
+  edicts: Edict[];
+}
+
+const Spring = {
+  name: 'Spring',
+  length: 8,
+  edicts: [Edict.A, Edict.B]
+}
+
+const Summer = {
+  name: 'Summer',
+  length: 8,
+  edicts: [Edict.B, Edict.C]
+}
+
+const Autumn = {
+  name: 'Autumn',
+  length: 7,
+  edicts: [Edict.C, Edict.D]
+}
+
+const Winter = {
+  name: 'Winter',
+  length: 6,
+  edicts: [Edict.D, Edict.A]
+}
+
+const Seasons = [
+  Spring,
+  Summer,
+  Autumn,
+  Winter
+]
+
+export default Seasons;
