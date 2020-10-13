23a621d62d80b22c4661540c9758d55db5670893
Iwan <iwanfrancis@gmail.com>
2020-10-13 18:23:26 +0100

    Make sure card shapes are centered within their containers

--- src/components/CurrentCard/CardShape/CardShape.tsx
+++ src/components/CurrentCard/CardShape/CardShape.tsx
@@ -1,20 +1,51 @@
 import classNames from "classnames";
 import React from "react";
-import { Shape } from "../../../constants/Card";
+import { Shape, ShapeRotation } from "../../../constants/Card";
 import styles from "./CardShape.module.scss";
 
 export interface Props {
   shape: Shape;
 }
 
+interface ShapeDimensions {
+  topLeftX: number;
+  topLeftY: number;
+  bottomRightX: number;
+  bottomRightY: number 
+}
+
 export default class CardShape extends React.PureComponent<Props> {
-  render() {
-    const shape = this.props.shape[0];
-    const squares = [];
+
+  findDimensions(shape: ShapeRotation): ShapeDimensions {
+    let topLeftX = shape[0].length;
+    let topLeftY = shape.length;
+    let bottomRightX = 0;
+    let bottomRightY = 0;
 
     for (let row = 0; row < shape[0].length; row++) {
-      const squareRow = [];
       for (let column = 0; column < shape.length; column++) {
+        if (shape[row][column] === 1) {
+          if (column > bottomRightX) bottomRightX = column; 
+          if (column < topLeftX) topLeftX = column;
+          if (row > bottomRightY) bottomRightY = row;
+          if (row < topLeftY) topLeftY = row;
+        }
+      }
+    }
+
+    return {topLeftX, topLeftY, bottomRightX, bottomRightY}
+  }
+
+  render() {
+    const shape = this.props.shape[0];
+    const dimensions = this.findDimensions(shape);
+    const squares = [];
+
+    console.log(dimensions);
+
+    for (let row = dimensions.topLeftY; row <= dimensions.bottomRightY; row++) {
+      const squareRow = [];
+      for (let column = dimensions.topLeftX; column <= dimensions.bottomRightX; column++) {
         const squareClass = shape[row][column] === 1 ? 
           classNames(styles.gray, styles.square) : classNames(styles.transparent, styles.square)
         squareRow.push(
