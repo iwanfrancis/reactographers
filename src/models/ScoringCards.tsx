d63959761357f8c709a701aaf0a64f84843ef52a
Iwan <iwanfrancis@gmail.com>
2020-10-16 19:08:03 +0100

    Add Mages Valley scoring card

--- src/models/ScoringCards.tsx
+++ src/models/ScoringCards.tsx
@@ -31,19 +31,53 @@ const TheCauldrons: ScoringCard = {
   score: (mapData: MapData) => {
     let reputation = 0;
     mapData.applyScoringFunction((gridPos: GridPosition, terrain: Terrain) => {
-      const adjacent = mapData.getAdjacentSquares(gridPos);
-      const adjacentSquaresFilled = Object.values(adjacent).every(square => square !== Terrain.Empty)
-      if (adjacentSquaresFilled && terrain === Terrain.Empty) reputation++;
+      if (terrain === Terrain.Empty) {
+        const adjacent = mapData.getAdjacentSquares(gridPos);
+        const adjacentSquaresFilled = Object.values(adjacent).every(square => square !== Terrain.Empty)
+        if (adjacentSquaresFilled) reputation++;
+      }
     })
-    console.log(reputation);
+    console.log('The Cauldrons', reputation);
     return reputation;
   }
 }
+
+const MagesValley: ScoringCard = {
+  type: ScoringCardType.Spacial,
+  name: 'Mages Valley',
+  text: [
+    'Earn two reputation stars for each water space adjacent to a mountain space.',
+    'Earn one reputation star for each farm space adjacent to a mountain space'
+  ],
+  diagram: <div></div>,
+  singlePlayerScore: 22,
+  score: (mapData: MapData) => {
+    let reputation = 0;
+    mapData.applyScoringFunction((gridPos: GridPosition, terrain: Terrain) => {
+      if (terrain === Terrain.Mountain) {
+        const adjacent = mapData.getAdjacentSquares(gridPos);
+        const pointsForMountain = Object.values(adjacent).reduce((points: number, terrain: Terrain) => {
+          if (terrain === Terrain.Water) return points + 2;
+          else if (terrain === Terrain.Farm) return points + 1;
+          else return points;
+        }, 0);
+
+        reputation = reputation + pointsForMountain;
+      }
+    })
+    console.log('Mages Valley: ', reputation);
+    return reputation;
+  }
+}
+
+
 
 export const ForestScoringCards = [];
 export const VillageScoringCards = [];
 export const SpacialScoringCards = [
   TheCauldrons
 ];
-export const FarmAndSeaScoringCards = [];
+export const FarmAndSeaScoringCards = [
+  MagesValley
+];
 
