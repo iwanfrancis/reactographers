ee72a35536bda670fb7e83cde0623309bd1a8791
Iwan <iwanfrancis@gmail.com>
2020-10-31 18:27:08 +0000

    Show ruins symbol on drawn card component

--- src/ui-components/Cards/DrawnCard/DrawnCard.tsx
+++ src/ui-components/Cards/DrawnCard/DrawnCard.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
-import { Card } from '../../../models/Card';
+import { Card, isRuinsCard, isShapeCard } from '../../../models/Card';
 import styles from './DrawnCard.module.scss';
+import { ReactComponent as Ruin } from '../../../assets/sprites/ruin/ruin.svg';
 
 export interface Props {
   card: Card;
@@ -20,7 +21,8 @@ export default class DrawnCard extends React.PureComponent<Props> {
     return (
       <div className={styles.card} style={offsetStyle}>
         <div className={styles.header}>
-          {card.time} {card.name}
+          {isRuinsCard(card) && <Ruin className={styles['ruins-header']}></Ruin>}
+          {isShapeCard(card) && card.time}
         </div>
       </div>
     )
