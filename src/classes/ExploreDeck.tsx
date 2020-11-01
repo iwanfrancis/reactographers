fa7bec55349b14513df66088831b5b8f2b0d5bf0
Iwan <iwanfrancis@gmail.com>
2020-11-01 15:16:21 +0000

    Handle ambush cards in explore deck

--- src/classes/ExploreDeck.tsx
+++ src/classes/ExploreDeck.tsx
@@ -1,55 +1,65 @@
-import { Card, isShapeCard, Shape } from "../models/Card";
+import { AmbushCard, Card, isAmbushCard, isShapeCard, Shape } from "../models/Card";
 import ExploreCards from "../game-components/ExploreCards";
 import { shuffleArray } from "../utils/shuffle";
+import AmbushCards from "../game-components/AmbushCards";
 
 export default class ExploreDeck {
-    cards: Card[] = ExploreCards.slice();
-    drawnCards: Card[] = []
+  cards: Card[] = ExploreCards.slice();
+  drawnCards: Card[] = [];
+  ambushCards: AmbushCard[] = shuffleArray(AmbushCards.slice());
 
-    constructor() {
-        this.shuffle();
+  constructor() {
+    if (this.ambushCards.length > 0) {
+      this.cards.push(this.ambushCards.pop() as AmbushCard);
+    }
+    this.shuffle();
+  }
+
+  public shuffle(): void {
+    this.cards = shuffleArray(this.cards);
+  }
+
+  public draw(): Card {
+    const drawnCard = this.cards.pop();
+    if (drawnCard) {
+      this.drawnCards.push(drawnCard);
+      return drawnCard;
+    } else {
+      return this.drawnCards[this.drawnCards.length - 1];
+    }
+  }
+
+  public reset(): void {
+    this.drawnCards = this.drawnCards.filter((card) => !isAmbushCard(card));
+    this.cards = this.cards.concat(this.drawnCards);
+    this.drawnCards = [];
+
+    if (this.ambushCards.length > 0) {
+      this.cards.push(this.ambushCards.pop() as AmbushCard);
     }
 
-    public shuffle(): void {
-        this.cards = shuffleArray(this.cards);
-    }
+    this.shuffle();
+  }
 
-    public draw(): Card {
-        const drawnCard = this.cards.pop();
-        if (drawnCard) {
-            this.drawnCards.push(drawnCard);
-            return drawnCard
-        } else {
-            return this.drawnCards[this.drawnCards.length - 1];
-        }
-        
-    }
+  public getCurrentCard(): Card {
+    return this.drawnCards[this.drawnCards.length - 1];
+  }
 
-    public reset(): void {
-        this.cards = this.cards.concat(this.drawnCards);
-        this.drawnCards = []
-        this.shuffle();
-    }
+  public getPreviousCards(): Card[] {
+    return this.drawnCards.slice(0, this.drawnCards.length - 1);
+  }
 
-    public getCurrentCard(): Card {
-        return this.drawnCards[this.drawnCards.length - 1]
-    }
+  public getTotalTime(): number {
+    return this.drawnCards.reduce((total: number, card: Card) => total + card.time, 0);
+  }
 
-    public getPreviousCards(): Card[] {
-        return this.drawnCards.slice(0, this.drawnCards.length - 1);
+  public currentShapeHasCoin(shape: Shape): boolean {
+    const currentCard = this.getCurrentCard();
+    if (isShapeCard(currentCard) && currentCard.coinIndex !== undefined) {
+      if (currentCard.shapes[currentCard.coinIndex] === shape) {
+        return true;
+      }
     }
-
-    public getTotalTime(): number {
-        return this.drawnCards.reduce((total: number, card: Card) => total + card.time, 0)
-    }
-
-    public currentShapeHasCoin(shape: Shape): boolean {
-        const currentCard = this.getCurrentCard();
-        if (isShapeCard(currentCard) && currentCard.coinIndex !== undefined) {
-            if (currentCard.shapes[currentCard.coinIndex] === shape) {
-                return true;
-            } 
-        }
-        return false;
-    }
+    return false;
+  }
 }
